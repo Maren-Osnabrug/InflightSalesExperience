7fa72181a962b7cff334beac125bd63e01eb3b49
Maren Osnabrug <maren.osnabrug@hva.nl>
2017-09-27 10:40:20 +0200

    first version of db

--- ISX/ISX/ViewController.swift
+++ ISX/ISX/ViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    var rootRef: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        rootRef = Database.database().reference()
+        let productsRef = Database.database().reference(withPath: "dataroot")
+        rootRef.observe(.value, with: { snapshot in
+//            print(snapshot.value)
+        })
+        productsRef.observe(.value, with: { snapshot in
+//            print("test", snapshot.value)
+            
+            for item in snapshot.children {
+                let products = item
+                print(type(of:products))
+                print(products)
+            }
+        })
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 

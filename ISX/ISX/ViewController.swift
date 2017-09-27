c23d5b5b56be71c628544e4c16f90ea5d9b27b10
zwiersj003 <jasper.zwiers16@gmail.com>
2017-09-27 16:12:44 +0200

    Login works; Link to landing page is missing

--- ISX/ISX/ViewController.swift
+++ ISX/ISX/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import FirebaseDatabase
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
     var rootRef: DatabaseReference!
@@ -36,7 +38,45 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //Login
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    /*
+     * Login function
+     */
+ 
+    @IBAction func loginAction(_ sender: Any) {
+        if self.emailTextField.text == "" || self.passwordTextField.text == "" {
+            
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            let alertController = UIAlertController(title: "Foutmelding", message: "Voer alstublieft een emailadres en wachtwoord in.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "Oke", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            
+            Auth.auth().signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
+                if error == nil {
+                    
+                    //Should be a redirect to controller for cabin crew
+//                    //Go to the HomeViewController if the login is sucessful
+//                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+//                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Foutmelding", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "Oke", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                }
+            }
+        }
+    }
 }
 

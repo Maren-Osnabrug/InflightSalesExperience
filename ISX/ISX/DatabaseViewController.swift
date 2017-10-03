52561e6177bcec8ac09a92a92bc34d96504b0d0d
Maren Osnabrug <maren.osnabrug@hva.nl>
2017-10-03 10:58:30 +0200

    commiting changes in different VC for safety

---
+++ ISX/ISX/DatabaseViewController.swift
@@ -0,0 +1,69 @@
+//
+//  DatabaseViewController.swift
+//  ISX
+//
+//  Created by Maren Osnabrug on 03-10-17.
+//  Copyright © 2017 Maren Osnabrug. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import FirebaseDatabase
+
+class DatabaseViewController: UIViewController {
+    var productsArray = [Product]()
+    var productGroupsArray = [AnyObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let datarootRef = Database.database().reference(withPath: "dataroot")
+        let productGroupsRef = datarootRef.child("productGroups")
+        let productsRef = datarootRef.child("products")
+        
+        productsRef.observe(.value, with: { snapshot in
+            for item in snapshot.children {
+                if let product = item as? DataSnapshot {
+                    let modelProduct = Product.init(snapshot: product)
+                    print(modelProduct.title)
+                    self.productsArray.append(modelProduct)
+                }
+            }
+        })
+        
+        productGroupsRef.observe(.value, with: { snapshot in
+            for item in snapshot.children {
+                self.productGroupsArray.append(item as AnyObject)
+                print(item)
+            }
+        })
+        
+        
+        //        datarootRef.observe(.value, with: { snapshot in
+        //            for item in snapshot.children {
+        //                let products = item as! DataSnapshot
+        //                let productAO = (products.value as! NSArray) as Array
+        //                for (product) in productAO.enumerated() {
+        //                    let modelProduct = Product.init(snapshotValue: product as AnyObject)
+        ////                    print(modelProduct.title)
+        //                    self.productsArray.append(modelProduct)
+        //                    print(self.productsArray.count)
+        //                }
+        //            }
+        //        })
+        //        print("total", self.productsArray.count)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        print(self.productsArray, self.productGroupsArray)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}

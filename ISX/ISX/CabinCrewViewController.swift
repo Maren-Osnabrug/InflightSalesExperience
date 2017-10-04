1644157ebeaeb3317bea2508c1f2d7f265c87e0a
Maren Osnabrug <maren.osnabrug@hva.nl>
2017-10-04 15:42:48 +0200

    UI for functionality

---
+++ ISX/ISX/CabinCrewViewController.swift
@@ -0,0 +1,122 @@
+//
+//  CabinCrewViewController.swift
+//  ISX
+//
+//  Created by Maren Osnabrug on 04-10-17.
+//  Copyright © 2017 Maren Osnabrug. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CabinCrewViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
+    }
+    
+    // Set the spacing between sections
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 15
+    }
+    
+    // Make the background color show through
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+    
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomRequestCell", for: indexPath) as! RequestCell
+        
+//        cell.productName
+//        cell.productCode
+        
+        cell.cellContentView.layer.borderColor = UIColor.white.cgColor
+        cell.cellContentView.layer.borderWidth = 1
+        
+        cell.chairView.layer.cornerRadius = 15
+        cell.chairView.layer.borderColor = UIColor.clear.cgColor
+        cell.chairView.layer.borderWidth = 1
+        cell.chairView.layer.shadowColor = UIColor.lightGray.cgColor
+        cell.chairView.layer.shadowOpacity = 0.8
+        cell.chairView.layer.shadowRadius = 3
+        cell.chairView.layer.shadowOffset = CGSize.init(width: 1, height: 1)
+        cell.chairLabel.text = "19A"
+        cell.productImage.backgroundColor = UIColor.lightGray
+
+        return cell
+     }
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ ISX/ISX/RequestCell.swift
@@ -0,0 +1,21 @@
+//
+//  RequestCell.swift
+//  ISX
+//
+//  Created by Maren Osnabrug on 04-10-17.
+//  Copyright © 2017 Maren Osnabrug. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RequestCell: UITableViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productCode: UILabel!
+    @IBOutlet weak var passengerLabel: UILabel!
+    @IBOutlet weak var chairView: UIView!
+    @IBOutlet weak var chairLabel: UILabel!
+    @IBOutlet weak var cellContentView: UIView!
+    
+}

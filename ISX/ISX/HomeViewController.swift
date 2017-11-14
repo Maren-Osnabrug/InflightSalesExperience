d0b287c306a7be5bca79c478e6ad8144f0976d80
Maren Osnabrug <maren.osnabrug@hva.nl>
2017-11-14 17:35:34 +0100

    added bestsellers in home and rewrote to headerview

--- ISX/ISX/HomeViewController.swift
+++ ISX/ISX/HomeViewController.swift
@@ -10,26 +10,22 @@ import Foundation
 import UIKit
 import FirebaseDatabase
 
-extension UIView {
-    func addBottomBorder(color: UIColor, width: CGFloat) {
-        let border = CALayer()
-        border.backgroundColor = color.cgColor
-        border.frame = CGRect(x: 0, y: frame.size.height - width, width: frame.size.width, height: width)
-        layer.addSublayer(border)
-    }
-}
+//extension UIView {
+//    func addBottomBorder(color: UIColor, width: CGFloat) {
+//        let border = CALayer()
+//        border.backgroundColor = color.cgColor
+//        border.frame = CGRect(x: 0, y: frame.size.height - width, width: frame.size.width, height: width)
+//        layer.addSublayer(border)
+//    }
+//}
 
-class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var suggestionLabel: UILabel!
-    @IBOutlet weak var suggestionProductsCollectionView: UICollectionView!
+class HomeViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     var datarootRef: DatabaseReference?
     var productsRef: DatabaseReference?
     
-    var productsArray = [Product]()
-    var suggestionProductsArray = [Product]()
+    var suggestedProductsArray = [Product]()
+    var bestSellers = ["40780", "45118", "45411", "15129", "15128", "10110", "11808", "10048", "10109", "10053", "68112"]
     var selectedProduct: Product?
     private let viewName = "Home"
     
@@ -38,38 +34,40 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
         // Do any additional setup after loading the view, typically from a nib.
         
         GoogleAnalyticsHelper().googleAnalyticLogScreen(screen: viewName)
-        suggestionProductsCollectionView.delegate = self
-        suggestionProductsCollectionView.dataSource = self
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
         
         configureDatabase()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        suggestionLabel.addBottomBorder(color: Constants.grey, width: 1)
-    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        suggestionLabel.addBottomBorder(color: Constants.grey, width: 1)
+//    }
     
     func configureDatabase() {
         datarootRef = Database.database().reference(withPath: "dataroot")
         productsRef = datarootRef?.child("products")
         productsRef?.observe(.value, with: { snapshot in
-            for (_, item) in snapshot.children.enumerated() {
+            for item in snapshot.children {
                 if let product = item as? DataSnapshot {
                     let modelProduct = Product.init(snapshot: product)
-                    self.productsArray.append(modelProduct)
-                    self.suggestionProductsArray = Array(self.productsArray.prefix(4))
+                    if (self.bestSellers.contains(modelProduct.id)) {
+                        self.suggestedProductsArray.append(modelProduct)
+                    }
+                    
                 }
             }
-            self.suggestionProductsCollectionView.reloadData()
+            self.collectionView?.reloadData()
         })
     }
     
-    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return suggestionProductsArray.count
+    public override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return suggestedProductsArray.count
     }
     
-    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    public override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "suggestionCell", for: indexPath) as? SuggestionCell else { return UICollectionViewCell() }
-        cell.setupData(product: productsArray[indexPath.row])
+        cell.setupData(product: suggestedProductsArray[indexPath.row])
 
         return cell
     }
@@ -79,9 +77,23 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
         return CGSize(width: itemSize, height: itemSize*Constants.multiplierFactorCollectionViewCell)
     }
     
-    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        selectedProduct = suggestionProductsArray[indexPath.row]
+    public override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedProduct = suggestedProductsArray[indexPath.row]
         performSegue(withIdentifier: "homeToProductSegue", sender: self)
+    }
+
+    override func collectionView(_ collectionView: UICollectionView,
+                                 viewForSupplementaryElementOfKind kind: String,
+                                 at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionElementKindSectionHeader:
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                             withReuseIdentifier: "HomeCollectionViewHeader",
+                                                                             for: indexPath) as! HomeCollectionViewHeader
+            return headerView
+        default:
+            assert(false, "Unexpected element kind")
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {

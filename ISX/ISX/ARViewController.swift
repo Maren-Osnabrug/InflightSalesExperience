29a0b3e0700c54bf9801232f11cf66cef0be6921
Maren Osnabrug <maren.osnabrug@hva.nl>
2017-11-21 14:11:10 +0100

    working on pan

--- ISX/ISX/ARViewController.swift
+++ ISX/ISX/ARViewController.swift
@@ -13,6 +13,7 @@ import ARKit
 class ARViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     var product: Product?
+    var scene: SCNScene?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +23,17 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-
+        addProduct()
         addTapGestureToSceneView()
+        addPanGestureToSceneView()
+        addPinchGestureToSceneView()
     }
     
     func addProduct(x: Float = 0, y: Float = 0, z: Float = -0.2) {
         if let product = product {
-            let scene = SCNScene(named: "art.scnassets/\(product.id)/\(product.id).scn")!
-            scene.rootNode.position = SCNVector3(x, y, z)
-            let node = scene.rootNode.childNode(withName: "Cube_bake", recursively: true)
+            scene = SCNScene(named: "art.scnassets/\(product.id)/\(product.id).scn")!
+            scene?.rootNode.position = SCNVector3(x, y, z)
+            let node = scene?.rootNode.childNode(withName: "Cube_bake", recursively: true)
             node?.position = SCNVector3(x, y, z)
             node?.eulerAngles.y = -.pi / 2
             sceneView.scene.rootNode.addChildNode(node!)
@@ -42,23 +45,64 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
     
-//    func addTapGestureToSceneView() {
-//        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.didTap(withGestureRecognizer:)))
-//        sceneView.addGestureRecognizer(tapGestureRecognizer)
-//    }
+    func addPanGestureToSceneView() {
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.didPan(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(panGestureRecognizer)
+    }
+    func addPinchGestureToSceneView(){
+        let pinchGesture: UIPinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(self.didPinch(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(pinchGesture)
+    }
     
     @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
-        let tapLocation = recognizer.location(in: sceneView)
-        let hitTestResults = sceneView.hitTest(tapLocation)
-        guard let node = hitTestResults.first?.node else {
-            let hitTestResultsWithFeaturePoints = sceneView.hitTest(tapLocation, types: .featurePoint)
-            if let hitTestResultWithFeaturePoints = hitTestResultsWithFeaturePoints.first {
-                let translation = hitTestResultWithFeaturePoints.worldTransform.translation
-                addProduct(x: translation.x, y: translation.y, z: translation.z)
+//        let tapLocation = recognizer.location(in: sceneView)
+//        let hitTestResults = sceneView.hitTest(tapLocation)
+//        guard let node = hitTestResults.first?.node else {
+//            let hitTestResultsWithFeaturePoints = sceneView.hitTest(tapLocation, types: .featurePoint)
+//            if let hitTestResultWithFeaturePoints = hitTestResultsWithFeaturePoints.first {
+//                let translation = hitTestResultWithFeaturePoints.worldTransform.translation
+//                addProduct(x: translation.x, y: translation.y, z: translation.z)
+//            }
+//            return
+//        }
+//        node.removeFromParentNode()
+    }
+    
+    func CGPointToSCNVector3(view: SCNView, depth: Float, point: CGPoint) -> SCNVector3 {
+        let locationWithz = SCNVector3Make(Float(point.x), Float(point.y), depth)
+        return view.unprojectPoint(locationWithz)
+    }
+    
+    @objc func didPan(withGestureRecognizer sender: UIGestureRecognizer) {
+        let point = sender.location(in: view)
+        let hitTestResults = sceneView.hitTest(point)
+        print("point : ", point)
+        guard let node = hitTestResults.first?.node else { return }
+        print("node : ", node.position.x , ", " , node.position.y)
+        
+//        let result: SCNVector3 = CGPointToSCNVector3(view: sceneView, depth: node.position.z, point: point)
+        let result: SCNVector3 = SCNVector3Make(Float(point.x), Float(point.y), 0.0)
+        node.position = result
+    }
+    
+    @objc func didPinch(withGestureRecognizer gesture: UIPinchGestureRecognizer) {
+        if let view = gesture.view {
+            switch gesture.state {
+            case .changed:
+                
+                let hitTestResults = sceneView.hitTest(gesture.location(in: view))
+                guard let node = hitTestResults.first?.node else { return }
+
+                let pinchScaleX = Float(gesture.scale) * node.scale.x
+                let pinchScaleY =  Float(gesture.scale) * node.scale.y
+                let pinchScaleZ =  Float(gesture.scale) * node.scale.z
+
+                node.scale = SCNVector3(pinchScaleX, pinchScaleY, pinchScaleZ)
+                gesture.scale = 1
+            default:
+                return
             }
-            return
         }
-        node.removeFromParentNode()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -108,3 +152,4 @@ extension float4x4 {
         return float3(translation.x, translation.y, translation.z)
     }
 }
+

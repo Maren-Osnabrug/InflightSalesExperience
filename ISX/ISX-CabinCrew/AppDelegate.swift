f2f5180dcfeb25517df7122f29ef98e5919bda0b
Maren Osnabrug <maren.osnabrug@hva.nl>
2017-12-02 22:48:32 +0100

    added in app handling for remote push notification

--- ISX/ISX-CabinCrew/AppDelegate.swift
+++ ISX/ISX-CabinCrew/AppDelegate.swift
@@ -14,6 +14,8 @@ import Firebase
 import FirebaseInstanceID
 import FirebaseMessaging
 
+import PopupDialog
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
     
@@ -56,35 +58,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         return true
     }
     
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
     // The callback to handle data message MessagingRemoteMessageces running iOS 10 or above.
     func application(received remoteMessage: MessagingRemoteMessage) {
         print(remoteMessage.appData)
-        let d : [String : Any] = remoteMessage.appData["notification"] as! [String : Any]
-        let body : String = d["body"] as! String
-        let controller = UIAlertController(title: "In App Notification", message: body, preferredStyle: .alert)
-        window?.rootViewController?.present(controller, animated: true, completion: nil)
     }
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
         print("Firebase registration token: \(fcmToken)")
@@ -101,20 +77,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         // Let FCM know about the message for analytics etc.
         Messaging.messaging().appDidReceiveMessage(userInfo)
-        print(userInfo)
-        print(userInfo["aps"])
-        print(userInfo["aps"]["alert"])
-        print(userInfo["aps"]["alert"])
-        if let aps = userInfo["aps"] as? [String:AnyObject] {
-            if let alert = aps["alert"] as? [String:AnyObject] {
-                if let message = alert["body"] as? [String:AnyObject] {
-                    let controller = UIAlertController(title: message["title"] as? String, message: message["body"] as? String, preferredStyle: .alert)
-                    window?.rootViewController?.present(controller, animated: true, completion: nil)
-                }
+
+        if let aps = userInfo["aps"] as? [String: AnyObject] {
+            if let alert = aps["alert"] as? [String: AnyObject] {
+                let title = alert["title"] as? String ?? "New Order"
+                let body = alert["body"] as? String ?? "A new order has come in"
+                showNewOrderPopup(title: title, body: body)
             }
         }
-        // handle your message
-
+    }
+    
+    func showNewOrderPopup(title: String, body: String) {
+        let popup = PopupDialog(title: title, message: body, buttonAlignment: .horizontal,
+                                transitionStyle: .bounceUp, gestureDismissal: true, hideStatusBar: true)
+        let buttonOne = DefaultButton(title: "Ok") {}
+        popup.addButton(buttonOne)
+        window?.rootViewController?.present(popup, animated: true, completion: nil)
     }
 }
 

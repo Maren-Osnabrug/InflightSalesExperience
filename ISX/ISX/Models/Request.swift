9bebb706213421db0705a6acca26c038e15f6ea2
Maren Osnabrug <maren.osnabrug@hva.nl>
2017-12-05 13:16:00 +0100

    added textfield so users can add flying blue number

--- ISX/ISX/Models/Request.swift
+++ ISX/ISX/Models/Request.swift
@@ -16,12 +16,16 @@ class Request {
     let customerChair: String
     var completed: Bool
     let ref: DatabaseReference?
+    var fbNumber: String = ""
+    var fbMiles: String = ""
     
-    init(id: Int, productId: Int, customerChair: String, completed: Bool) {
+    init(id: Int, productId: Int, customerChair: String, completed: Bool, fbNumber: String, fbMiles: String) {
         self.id = id
         self.productId = productId
         self.completed = completed
         self.customerChair = customerChair
+        self.fbMiles = fbMiles
+        self.fbNumber = fbNumber
         self.ref = nil
     }
     
@@ -31,6 +35,12 @@ class Request {
         self.productId = dict["product"] as! Int
         self.customerChair = dict["customerChair"] as! String
         self.completed = (dict["completed"] as! Int == 1 ? true : false)
+        if let miles = dict["fbMiles"] as? String {
+            self.fbMiles = miles
+        }
+        if let number = dict["fbNumber"] as? String {
+            self.fbNumber = number
+        }
         self.ref = snapshot.ref
     }
     
@@ -39,7 +49,9 @@ class Request {
             "id": id,
             "product": productId,
             "completed": completed,
-            "customerChair": customerChair
+            "customerChair": customerChair,
+            "fbMiles": fbMiles,
+            "fbNumber": fbNumber
         ]
     }
 }
